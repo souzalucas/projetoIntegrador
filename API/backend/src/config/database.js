1b3d788211489c3bf5d1df4ba5608890ab6d316e
alan <alansantana@alunos.utfpr.edu.br>
2019-12-02 17:21:25 -0300

    Adição tabela dias_semana(Turmas)

--- API/backend/src/config/database.js
+++ API/backend/src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: 'alan241198',
     database: 'estadio',
     operatorAliases: false,
     define: {
