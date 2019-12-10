ecd830ceed41fdb2f00828c8fbec7146d8c3f9c8
alan <alansantana@alunos.utfpr.edu.br>
2019-12-10 18:03:31 -0300

    Model e frontend turmas

--- API/backend/src/database/migrations/20191202163655-create-alunos.js
+++ API/backend/src/database/migrations/20191202163655-create-alunos.js
@@ -22,14 +22,14 @@ module.exports = {
       allowNull: false,
       type: Sequelize.STRING,
     },
-    turmaId:{
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {         
-        model: 'turmas',
-        key: 'id'
-      }
-    },
+    // turmaId:{
+    //   type: Sequelize.INTEGER,
+    //   allowNull: false,
+    //   references: {         
+    //     model: 'turmas',
+    //     key: 'id'
+    //   }
+    // },
     created_at: {
       allowNull: false,
       type: Sequelize.DATE,
